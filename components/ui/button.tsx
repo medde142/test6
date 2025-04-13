9389f0a0fc16d0c94847e33989450b665b9ab87b
medde142 <m.stapelbroek@web.de>
2025-04-13 12:42:52 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,1 +1,5 @@
-export const Button = ({ children, ...props }) => <button {...props} className='bg-black text-white py-2 px-4 rounded'>{children}</button>;
+export const Button = ({ children, ...props }) => (
+  <button {...props} className="bg-black text-white py-2 px-4 rounded">
+    {children}
+  </button>
+);
