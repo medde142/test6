c6b8159f41552a552fda0ddc964062d826932915
medde142 <m.stapelbroek@web.de>
2025-04-13 12:42:29 +0200

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,3 @@
-export const Input = (props) => <input {...props} className='border rounded px-2 py-1 w-full' />;
+export const Input = (props) => (
+  <input {...props} className="border rounded px-2 py-1 w-full" />
+);
