5d6328c754151e6261c4c10269e4c1dbf187c250
medde142 <m.stapelbroek@web.de>
2025-04-13 12:43:09 +0200

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,2 +1,5 @@
-export const Card = ({ children }) => <div className='border rounded-xl p-4 bg-white shadow'>{children}</div>;
+export const Card = ({ children }) => (
+  <div className="border rounded-xl p-4 bg-white shadow">{children}</div>
+);
+
 export const CardContent = ({ children }) => <div>{children}</div>;
